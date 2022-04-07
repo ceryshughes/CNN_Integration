14783d67ef5e5e559c24269d0d255eb31342cfa7
ceryshughes <ceryshughes@umass.edu>
2022-04-07 10:57:48 -0400

    Updated sampling code

--- WaveformCNN/laff_vcv/sample_params.py
+++ WaveformCNN/laff_vcv/sample_params.py
@@ -1,8 +1,17 @@
 import compute_vcv_distributions
 import random
 import statistics
+import csv #todo: convert klatt parameters to csv
 
-
+#Extra closure voicing synthesis parameters
+#These values are taken from the Kingston 2008 experimental stimuli values for the closure voicing set
+constant_synth_params = {"f0TransitionDur": 0.055, #in seconds
+                         "F1TransitionDur": 0.055,
+                         "OtherFsTransitionDur": 0.04,
+                         "VowelDur": 0.205,
+                         "F1Closure": 150, #value of F1 during closure voicing, Hz
+                         "f0Closure": 90 #value of f0 during closure voicing, Hz #todo: check these with John because they seem unintuitive
+                         }
 
 def normalize(tokens):
     #TODO
@@ -79,39 +88,81 @@ def generate_sample_vcv(tokens, label):
 
     return sampled_token
 
-def generate_metadata_file(out_file_name, tokens):
-    return
 
+#tokens: (string, Vcv object) pair
+# where the string is the fileID for the Vcv object
+# Outputs to out_file_name the fileIDs and stop labels of each token in tokens
+def generate_stop_metadata_file(out_file_name, tokens):
+    with open(out_file_name, 'w', newline='') as csvfile:
+        fieldnames = ["FileID", "Category"]
+        writer = csv.DictWriter(csvfile,fieldnames=fieldnames)
+        for token in tokens:
+            file_id = token[0]
+            values = token[1]
+            writer.writerow({"FileID":file_id, "Category": values.stop.label})
+
+
+#tokens: (string, Vcv object) pair
+# where the string is the fileID for the Vcv object
+# Outputs to out_file_name the fileIDs and stop labels of each token in tokens
 def generate_klatt_parameter_file(out_file_name, tokens):
-    return
+    with open(out_file_name, 'w', newline='') as csvfile:
+        fieldnames = ["Name","VowelDur","ClosureDur","F1offset", "F1steady","F2offset", "F2steady",
+                      "F3offset", "F3steady", "F4offset", "F4steady", "F5offset", "F5steady", "f0offset",
+                      "f0steady", "f0TransitionDur", "F1TransitionDur", "OtherFsTransitionDur",
+                      "F1Closure", "f0Closure", "ClosureVoicingDur"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        for token in tokens:
+            token_label = token[0]
+            data = token[1]
+            row = {"Name" : token_label,
+                    "VowelDur": constant_synth_params["VowelDur"],
+                    "ClosureDur" : data.stop.closure_dur,
+                    "F1offset": data.v1.measurement_dict[1]["transit"],
+                    "F1steady": data.v1.measurement_dict[1]["steady"],
+                    "F2offset": data.v1.measurement_dict[2]["transit"],
+                    "F2steady": data.v1.measurement_dict[2]["steady"],
+                    "F3offset": data.v1.measurement_dict[3]["transit"],
+                    "F3steady": data.v1.measurement_dict[3]["steady"],
+                    "F4offset": data.v1.measurement_dict[4]["transit"],
+                    "F4steady": data.v1.measurement_dict[4]["steady"],
+                    "F5offset": data.v1.measurement_dict[5]["transit"],
+                    "F5steady": data.v1.measurement_dict[5]["steady"],
+                    "f0offset": data.v1.measurement_dict[0]["transit"],
+                    "f0steady": data.v1.measurement_dict[0]["transit"],
+                    "f0TransitionDur": constant_synth_params["f0TransitionDur"],
+                    "F1TransitionDur": constant_synth_params["F1TransitionDur"],
+                    "OtherFsTransitionDur":constant_synth_params["OtherFsTransitionDur"],
+                    "F1Closure": constant_synth_params["F1Closure"],
+                    "f0Closure": constant_synth_params["f0Closure"],
+                    "ClosureVoicingDur": data.stop.voicing_dur
+                   }
+            writer.writerow(row)
+
 
 
 if __name__ == "__main__":
     num_samples_voiced = 500
     num_samples_voiceless = 500
+    metadata_fn = "sampled_stop_categories.csv"
+    klatt_param_fn = "sampled_stop_klatt_params.csv"
 
     #Get distributions
     voiced_dist, voiceless_dist = compute_vcv_distributions.get_vcv_data()
 
     #Randomly sample frequency and stop values
-    sampled_voiced_data = []
+    sampled_data = []
     for index in range(num_samples_voiced):
         sample = generate_sample_vcv(voiced_dist, "voiced")
         sample_id = str(index) + "_" + sample.speaker
-        sampled_voiced_data.append((sample_id, sample))
-    sampled_voiceless_data = []
+        sampled_data.append((sample_id, sample))
     for index in range(num_samples_voiceless):
         sample = generate_sample_vcv(voiceless_dist, "voiceless")
         sample_id = str(index) + "_" + sample.speaker
-        sampled_voiceless_data.append((sample_id,sample))
+        sampled_data.append((sample_id,sample))
+
+    #Write metadata and Klatt parameter files
+    generate_stop_metadata_file(metadata_fn, sampled_data)
+    generate_klatt_parameter_file(klatt_param_fn, sampled_data)
 
 
-
-
-
-
-#Write ID as name to Klatt parameter table file
-
-
-
-#Write ID and label to the metadata table file
